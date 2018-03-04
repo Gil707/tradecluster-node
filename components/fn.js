8a18e670f6f239c8af471a0a15d5765627918069
Gil <gil707@gmail.com>
2018-03-04 18:13:50 +0300

    split upload dir

--- components/fn.js
+++ components/fn.js
@@ -4,14 +4,17 @@ fn = function(req, res) {
 
 
     fs.readFile(req.files.upload.path, function (err, data) {
-        let newPath = __dirname + '/../public/uploads/' + req.files.upload.name;
+
+        let structure_path = req.url.split('?CKEditor=').pop().split('&CKE').shift();
+
+        let newPath = __dirname + '/../public/uploads/' + structure_path + '/' + req.files.upload.name;
         fs.writeFile(newPath, data, function (err) {
             if (err) console.log({err: err});
             else {
                 html = "";
                 html += "<script type='text/javascript'>";
                 html += "    var funcNum = " + req.query.CKEditorFuncNum + ";";
-                html += "    var url     = \"/uploads/" + req.files.upload.name + "\";";
+                html += "    var url     = \"/uploads/" + structure_path + "/" + req.files.upload.name + "\";";
                 html += "    var message = \"Uploaded file successfully\";";
                 html += "";
                 html += "    window.parent.CKEDITOR.tools.callFunction(funcNum, url, message);";
